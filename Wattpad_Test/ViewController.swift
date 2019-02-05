2020ab5bcc58e0eee8bdc815009037b62e828165
Rupika Sompalli <rupikasompalli@rupikas-mac-mini.local>
2019-02-05 11:07:40 -0500

    Initial Commit

---
+++ Wattpad_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wattpad_Test
+//
+//  Created by Rupika Sompalli on 05/02/19.
+//  Copyright © 2019 Venkata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
